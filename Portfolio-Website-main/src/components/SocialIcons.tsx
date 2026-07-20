96adea00042be659d28e95b4fe89309fff451a37
vansh <vansh1234@gmail.com>
2026-07-21 01:26:00 +0530

    Add resume link

--- Portfolio-Website-main/src/components/SocialIcons.tsx
+++ Portfolio-Website-main/src/components/SocialIcons.tsx
@@ -76,7 +76,7 @@ const SocialIcons = () => {
           </a>
         </span>
       </div>
-      <a className="resume-button" href="#">
+      <a className="resume-button" href="/resume.pdf">
         <HoverLinks text="RESUME" />
         <span>
           <TbNotes />
